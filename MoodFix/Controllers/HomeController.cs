e81b0e589afe7a555785cc519b71831d10f25fbc
Alex Matthewman <alex.matthewman@gmail.com>
2021-04-08 14:04:32 +1000

    Added Addiction section

--- MoodFix/Controllers/HomeController.cs
+++ MoodFix/Controllers/HomeController.cs
@@ -128,6 +128,10 @@ namespace MoodFix.Controllers
             return View();
         }
 
+        public IActionResult Addiction()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
